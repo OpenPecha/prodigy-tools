11d46fad8c08a7cccf5ccf4d413a777001dcf1cb
Jampa Dhondup <zakongjampa@gmail.com>
2022-11-28 19:08:35 +0000

    Trying to access one image and show the image

--- S3/get_image_list.py
+++ S3/get_image_list.py
@@ -1,19 +1,41 @@
 import boto3
+from PIL import Image
 
 def get_image_list(bucketname='image-processing.bdrc.io'):
     ''' From this method we will get the list of all the images from the bucket name'''
     session = boto3.Session(profile_name='jampa')
     s3 = session.resource('s3', region_name='us-east-1')
 
-    myBucket = s3.Bucket(bucketname)
+    # myBucket = s3.Bucket(bucketname)
 
-    for myBucketObject in myBucket.objects.all():
-        print(myBucketObject)
-    
+    # for myBucketObject in myBucket.objects.all():
+    #     print(myBucketObject)
+
+    image = s3.Object(bucketname,'/NLM1/W2KG208132/archive/W2KG208132-I2KG208184/I2KG2081840003.tif')
+
+def read_image_s3(bucketname, key):
+    '''Load and show image file from s3
+
+    Parameter
+    --------
+
+    bucketname :string      bucket name 
+    key :       string      Path in s3
+
+    '''
+    session = boto3.Session(profile_name='jampa')
+    s3 = session.resource('s3')
+
+    bucket = s3.Bucket(bucketname)
+    object  = bucket.Object(key)
+    responese = object.get()
+    file = responese['Body']
+    im = Image.open(file)
 
 
 if __name__ == "__main__":
-    get_image_list()
+    # get_image_list()
+    read_image_s3('image-processing.bdrc.io','/NLM1/W2KG208132/archive/W2KG208132-I2KG208184/I2KG2081840003.tif')
 
     # print(dir(s3.Bucket.all))
 
