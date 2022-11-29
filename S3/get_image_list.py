0cb72bf5e14c1c18135ff374c5bde8e2c29f43a0
eroux <elie.roux@telecom-bretagne.eu>
2022-11-29 10:49:13 +0100

    code works!

--- S3/get_image_list.py
+++ S3/get_image_list.py
@@ -1,64 +1,39 @@
 import boto3
+import botocore
+import io
+import logging
 from PIL import Image
 
-def get_image_list(bucketname='image-processing.bdrc.io'):
-    ''' From this method we will get the list of all the images from the bucket name'''
-    session = boto3.Session(profile_name='jampa')
-    s3 = session.resource('s3', region_name='us-east-1')
+logging.basicConfig(level=logging.INFO)
 
-    # myBucket = s3.Bucket(bucketname)
+SESSSION = boto3.Session(profile_name='imageprocessing')
+S3 = SESSSION.client('s3')
+BUCKET_NAME = "image-processing.bdrc.io"
 
-    # for myBucketObject in myBucket.objects.all():
-    #     print(myBucketObject)
+def get_s3_keys(prefix=""):
+    l = S3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=prefix)
+    keys = [o["Key"] for o in l["Contents"]]
+    return sorted(keys)
 
-    image = s3.Object(bucketname,'/NLM1/W2KG208132/archive/W2KG208132-I2KG208184/I2KG2081840003.tif')
+def gets3blob(s3Key):
+    f = io.BytesIO()
+    try:
+        S3.download_fileobj(BUCKET_NAME, s3Key, f)
+        return f
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            logging.info("cannot find s3 key %s", s3Key)
+            return None
+        else:
+            raise
 
-def read_image_s3(bucketname, key):
-    '''Load and show image file from s3
-
-    Parameter
-    --------
-
-    bucketname :string      bucket name 
-    key :       string      Path in s3
-
-    '''
-    session = boto3.Session(profile_name='jampa')
-    s3 = session.resource('s3')
-
-    bucket = s3.Bucket(bucketname)
-    object  = bucket.Object(key)
-    responese = object.get()
-    file = responese['Body']
-    im = Image.open(file)
+def read_image_s3(s3Key):
+    bbuf = gets3blob(s3Key)
+    if bbuf is None:
+        return None
+    return Image.open(bbuf)
 
 
 if __name__ == "__main__":
-    # get_image_list()
-    read_image_s3('image-processing.bdrc.io','/NLM1/W2KG208132/archive/W2KG208132-I2KG208184/I2KG2081840003.tif')
-
-    # print(dir(s3.Bucket.all))
-
-    # for bucket in s3.buckets.all():
-    #     print(bucket.name)
-
-
-
-# # session = boto3.Session(profile_name='jampa')
-# # s3 = session.resource('s3')
-# response = s3.list_buckets()
-# #Print out bucket name 
-# for bucket in response['Buckets']:
-# 	print(bucket['Name'])
-
-# # paginator = s3.get_paginator('list_objects')
-# # page_iterator = paginator.paginate(Bucket='image-processing.bdrc.io')
-
-# # Create a reusable Paginator
-# paginator = s3.get_paginator('list_objects')
-
-# # Create a PageIterator from the Paginator
-# page_iterator = paginator.paginate(Bucket='image-processing.bdrc.io')
-
-# for page in page_iterator:
-#     print(page['Contents'])
+    #print(get_s3_keys())
+    print(read_image_s3('NLM1/W2KG208132/archive/W2KG208132-I2KG208184/I2KG2081840003.tif'))
