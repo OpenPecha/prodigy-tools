19326e68447094fbf07fd9ed8b630a34609e0aeb
Jampa Dhondup <zakongjampa@gmail.com>
2022-11-30 17:26:22 +0000

    Resize the image

---
+++ S3/bucket.py
@@ -0,0 +1,21 @@
+import boto3
+
+
+
+class bucket:
+    ''' All the method related to s3 bucket and operation onto the buckets
+    Parameters 
+    -----------
+    
+    '''
+
+    def __init__(self, bucketname, ):
+       pass
+
+    def get_object_list(self):
+        pass 
+
+    def get_an_object(self):
+        pass
+    
+    
