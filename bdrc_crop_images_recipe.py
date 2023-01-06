1701d24940521c4281899ff6a39ecebf8191d09d
Tashi Tsering <ta3tsering@gmail.com>
2023-01-06 16:19:19 +0530

    recipe updated

--- bdrc_crop_images_recipe.py
+++ bdrc_crop_images_recipe.py
@@ -4,11 +4,24 @@ import json
 from prodigy.util import img_to_b64_uri
 from typing import List, Optional
 import os
+import logging
 
+# s3 cofig
+os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/home/ta4tsering/.aws/credentials"
+s3 = boto3.resource("s3")
+s3_client = boto3.client("s3")
+IMAGE_PROCESSING_BUCKET = "image-processing.bdrc.io"
 
+# log config 
+logging.basicConfig(
+filename="bdrc_crop_images.log",
+format="%(levelname)s: %(message)s",
+level=logging.INFO,
+)
 
 @prodigy.recipe("custom-recipe")
 def custom_recipe(dataset, s3_prefix):
+    logging.info(f"dataset:{dataset}, s3_prefix:{s3_prefix}")
     return {
         "dataset": dataset,
         "stream": stream_from_s3(s3_prefix),
@@ -20,11 +33,6 @@ def custom_recipe(dataset, s3_prefix):
 
 def stream_from_s3(s3_prefix):
     # Get all loaded images.
-    os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/home/ta4tsering/.aws/credentials"
-    s3 = boto3.resource("s3")
-    s3_client = boto3.client("s3")
-    IMAGE_PROCESSING_BUCKET = "image-processing.bdrc.io"
-    
     response = s3_client.list_objects_v2(Bucket=IMAGE_PROCESSING_BUCKET, Prefix=s3_prefix)
     if response:
         for info in response['Contents']:
@@ -32,6 +40,7 @@ def stream_from_s3(s3_prefix):
             # Read the image.
             obj = s3.Object(IMAGE_PROCESSING_BUCKET, img_key)
             img = obj.get()['Body'].read()
+            logging.info(f"s3_image_key: {img_key}")
 
             # Provide response that Prodigy expects.
             yield json.dumps({'image': img_to_b64_uri(img, 'image/jpg')})
