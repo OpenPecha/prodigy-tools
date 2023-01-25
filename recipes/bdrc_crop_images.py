027ace3d204b3b223d987f3c2c5904fad59fa4f4
Jampa Dhondup <zakongjampa@gmail.com>
2023-01-25 04:52:45 +0000

    Update the recipe to take max widt and override the prodigy config using the prodigy.json

--- recipes/bdrc_crop_images.py
+++ recipes/bdrc_crop_images.py
@@ -38,7 +38,10 @@ def bdrc_crop_images_recipe(dataset, s3_prefix):
         "stream": stream_from_s3(obj_keys),
         "view_id": "image_manual",
         "config": {
-            "labels": ["PAGE"]
+            "labels": ["PAGE"], 
+            "custom_theme" :  {"cardMaxWidth": width},
+            "exclude_by": "input",
+            "auto_count_stream": True,
         }
     }
 
