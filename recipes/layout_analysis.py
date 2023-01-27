ea446d12ac9aa789998162c44242f95423dd4c1f
Tashi Tsering <tashitsering@Tashis-MacBook-Pro.local>
2023-01-27 15:18:44 +0530

    fix issue #13

--- recipes/layout_analysis.py
+++ recipes/layout_analysis.py
@@ -50,4 +50,5 @@ def stream_from_s3(obj_keys):
             Params={"Bucket": BUCKET_NAME, "Key": obj_key},
             ExpiresIn=31536000
         )
-        yield {"image": image_url}
+        image_id = (obj_key.split("/"))[-1]
+        yield {"id": image_id, "image": image_url}
