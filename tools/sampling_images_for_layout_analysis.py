6527cf2fb1277a12d660144579156670fbf8862e
Tashi Tsering <tashitsering@Tashis-MacBook-Pro.local>
2023-02-02 17:38:35 +0530

    fix #8

--- tools/sampling_images_for_layout_analysis.py
+++ tools/sampling_images_for_layout_analysis.py
@@ -1,14 +1,9 @@
 import os
 import boto3
 import hashlib
-import requests
 from pathlib import Path
-from PIL import Image
-import logging
-import rdflib
-from rdflib import URIRef, Graph
-from rdflib.namespace import Namespace, NamespaceManager
-from openpecha.buda.api import image_group_to_folder_name
+from git import Repo
+
 
 # s3 config
 os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "~/.aws/credentials"
@@ -18,97 +13,72 @@ BUCKET_NAME = "archive.tbrc.org"
 s3_bucket = s3.Bucket(BUCKET_NAME)
 
 
-BDR = Namespace("http://purl.bdrc.io/resource/")
-BDO = Namespace("http://purl.bdrc.io/ontology/core/")
-NSM = NamespaceManager(rdflib.Graph())
+def get_s3_keys_of_unique_images(unique_images, s3_images_list):
+    unique_images_s3_keys = []
+    for s3_image in s3_images_list:
+        image_name = ((s3_image.split("/"))[-1]).split(".")[0]
+        if image_name in unique_images:
+            unique_images_s3_keys.append(s3_image)
+    return unique_images_s3_keys
 
 
-def get_number_of_intro_images(imagegroup):
-
-    ttl_response = requests.get(f"http://purl.bdrc.io/resource/{imagegroup}.ttl")
-    ttl_content = ttl_response.text
-    g = Graph()
-    try:
-        g.parse(data=ttl_content, format="ttl")
-        number = int((g.value(BDR[imagegroup], BDO["volumePagesTbrcIntro"])).split("/")[-1])
-        return number
-    except:
-        return None
-
-def get_sample_images_s3_key(imagegroup, s3_prefix):
-
-    sample_images_s3_keys = []
-    images_list = []
-    number_of_intro = get_number_of_intro_images(imagegroup)
-    response = requests.get(
-        f"https://iiifpres.bdrc.io/il/v:bdr:{imagegroup}"
-    )
-    file_json = response.json()
-
-    if number_of_intro:
-        start = number_of_intro
-    else:
-        start = 0
-
-    for info in file_json[start:start+10]:
-        images_list.append(info['filename'])
-    for info in file_json[-10:]:
-        images_list.append(info['filename'])
-
-    for image_name in images_list:
-        s3_key = f"{s3_prefix}/{image_name}"
-        sample_images_s3_keys.append(s3_key)
-
-    return sample_images_s3_keys
-    
-
-def get_s3_prefix( work_local_id, imagegroup):
-
-    md5 = hashlib.md5(str.encode(work_local_id))
+def get_s3_images_list_of_work(work_id):
+    obj_keys = []
+    md5 = hashlib.md5(str.encode(work_id))
     two = md5.hexdigest()[:2]
+    prefix = f"Works/{two}/{work_id}/images"
+    continuation_token = None
+    while True:
+        if continuation_token:
+            response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=prefix, ContinuationToken=continuation_token)
+        else:
+            response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=prefix)
+        if response['Contents']:
+            for obj in response['Contents']:
+                obj_key = obj['Key']
+                obj_keys.append(obj_key)
+        continuation_token = response.get("NextContinuationToken")
+        if not continuation_token:
+            break
+    return obj_keys
 
-    vol_folder = image_group_to_folder_name(work_id, imagegroup)
-    base_dir = f"Works/{two}/{work_local_id}"
-    return f"{base_dir}/images/{vol_folder}"
 
-def get_value(json_node):
-    if json_node["type"] == "literal":
-        return json_node["value"]
-    else:
-        return NSM.qname(URIRef(json_node["value"]))
+def get_list_of_unique_images(repo_path):
+    images_list = []
+    unique_images_path = Path(f"{repo_path}/unique_images")
+    images_path_list = list(unique_images_path.iterdir())
+    for images_path in images_path_list:
+        images_list.append(images_path.stem)
+    return images_list
+
+def get_branch(repo, branch):
+    if branch in repo.heads:
+        return branch
+    return "master"
+
+def download_repo(repo_name, out_path=None, branch="master"):
+    pecha_url = f"https://github.com/MonlamAI/{repo_name}.git"
+    out_path = Path(out_path)
+    out_path.mkdir(exist_ok=True, parents=True)
+    repo_path = out_path / repo_name
+    Repo.clone_from(pecha_url, str(repo_path))
+    repo = Repo(str(repo_path))
+    branch_to_pull = get_branch(repo, branch)
+    repo.git.checkout(branch_to_pull)
+    return repo_path
 
 
-def get_volume_infos(work_prefix_url):
-    response = requests.get(
-        f"http://purl.bdrc.io/query/table/volumesForWork?R_RES={work_prefix_url}&format=json&pageSize=500"
-    )
-    if response.status_code != 200:
-        print(
-            f"Volume Info Error: No info found for Work {work_prefix_url}: status code: {response.status_code}"
-        )
-        return
-    res = response.json()
-    for b in res["results"]["bindings"]:
-        volume_prefix_url = NSM.qname(URIRef(b["volid"]["value"]))
-        yield {
-            "vol_num": get_value(b["volnum"]),
-            "volume_prefix_url": volume_prefix_url,
-            "imagegroup": volume_prefix_url[4:],
-        }
+def get_unique_images_keys(repo_name, work_id):
+    repo_path = download_repo(repo_name, "./")
+    unique_images = get_list_of_unique_images(repo_path)
+    s3_images_list = get_s3_images_list_of_work(work_id)
+    unique_images_s3_keys = get_s3_keys_of_unique_images(unique_images, s3_images_list)
+    
+    return unique_images_s3_keys
 
-def get_sample_images_dict(work_id):
-    curr ={}
-    final_sample_dict = {}
-    for _, vol_info in enumerate(get_volume_infos(f"bdr:{work_id}")):
-        imagegroup = vol_info['imagegroup']
-        s3_prefix = get_s3_prefix(work_id, imagegroup)
-        sample_images_s3_key = get_sample_images_s3_key(imagegroup, s3_prefix)
-        curr[imagegroup]=sample_images_s3_key
-        final_sample_dict.update(curr)
-        curr = {}
-    return final_sample_dict
 
 if __name__ == "__main__":
-    work_id = "W00EGS1016255"
-    finale_sample_dict = get_sample_images_dict(work_id)
-    print(finale_sample_dict)
+    repo_name = "OCRtest"
+    work_id = "W26071"
+    unique_images_s3_keys = get_unique_images_keys(repo_name, work_id)
+    print(unique_images_s3_keys)
