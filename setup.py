e494c2ff7182816076bc78bd27b6b8b359bec707
Tashi Tsering <tashitsering@Tashis-MacBook-Pro-2.local>
2023-03-15 12:50:15 +0530

    add sample images

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
 
-setup(
-    name="prodigy-tools",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=[
-        "boto3>=1.24.50, <2.0",
-        "Pillow>=8.4.0, <9.0"
-    ],
-)
+# setup(
+#     name="prodigy-tools",
+#     version="0.0.1",
+#     packages=find_packages(),
+#     install_requires=[
+#         "boto3>=1.24.50, <2.0",
+#         "Pillow>=8.4.0, <9.0"
+#     ],
+# )
