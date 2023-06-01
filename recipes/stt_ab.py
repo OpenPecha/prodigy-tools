b05b46a776527a307c9df85bd83a75c0916dd660
Tashi Tsering <tashitsering@Tashis-MacBook-Pro-2.local>
2023-06-01 15:53:00 +0530

    add stt ab instance

---
+++ recipes/stt_ab.py
@@ -0,0 +1,52 @@
+
+import logging
+import prodigy
+import jsonlines
+
+# log config 
+logging.basicConfig(
+    filename="/usr/local/prodigy/logs/stt_ab.log",
+    format="%(levelname)s: %(message)s",
+    level=logging.INFO,
+    )
+
+# Prodigy has a logger named "prodigy" according to 
+# https://support.prodi.gy/t/how-to-write-log-data-to-file/1427/10
+prodigy_logger = logging.getLogger('prodigy')
+prodigy_logger.setLevel(logging.INFO)
+
+@prodigy.recipe("stt-ab-recipe")
+def stt_ab_recipe(dataset, jsonl_file):
+    logging.info(f"dataset:{dataset}, jsonl_file_path:{jsonl_file}")
+    blocks = [
+        {"view_id": "audio"},
+        {
+            "view_id": "text_input",
+            "field_rows": 6,
+            "field_label": "Transcript",
+            "field_id": "transcript",
+            "field_autofocus": True,
+        },
+        {
+            "view_id": "html",
+            "html_template" : "<button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.5)'>0.5x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.7)'>0.7x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1)'>1x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.3)'>1.3x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.5)'>1.5x speed</button>"
+        }
+    ]
+    return {
+        "dataset": dataset,
+        "stream": stream_from_jsonl(jsonl_file),
+        "view_id": "blocks",
+        "config": {
+            "blocks": blocks,
+            "editable": True
+        }
+    }
+
+
+def stream_from_jsonl(jsonl_file):
+    with jsonlines.open(jsonl_file) as reader:
+        for line in reader:
+            audio_id = line["id"]
+            audio_url = line["audio_url"]
+            text = line["user_input"]
+            yield {"id": audio_id, "audio": audio_url, "user_input": text}
