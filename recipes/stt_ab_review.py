d00cc754462665b85ad247ee372e3cb3ac91ddf5
Tenzin Gayche <gaychetenzin@gmail.com>
2023-06-08 02:39:45 +0530

    added new ab recipe and service

---
+++ recipes/stt_ab_review.py
@@ -0,0 +1,69 @@
+import logging
+import prodigy
+import sqlite3
+import json
+
+# log config
+logging.basicConfig(
+    filename="/usr/local/prodigy/logs/stt_ab_review.log",
+    format="%(levelname)s: %(message)s",
+    level=logging.INFO,
+)
+
+# Prodigy has a logger named "prodigy" according to
+# https://support.prodi.gy/t/how-to-write-log-data-to-file/1427/10
+prodigy_logger = logging.getLogger("prodigy")
+prodigy_logger.setLevel(logging.INFO)
+
+
+@prodigy.recipe("stt-ab-review-recipe")
+def stt_ab_recipe(dataset, database):
+    logging.info(f"dataset:{dataset}")
+    blocks = [
+        {"view_id": "audio"},
+        {"view_id": "text_input"},
+        {
+            "view_id": "html",
+            "html_template": "<button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.5)'>0.5x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.7)'>0.7x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1)'>1x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.3)'>1.3x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.5)'>1.5x speed</button>",
+        },
+    ]
+    return {
+        "dataset": dataset,
+        "stream": stream_from_sqlite(dataset, database),
+        "view_id": "blocks",
+        "config": {"blocks": blocks, "editable": True},
+    }
+
+
+def stream_from_sqlite(dataset, database):
+    # Connect to SQLite database
+    connection = sqlite3.connect(f"{database}")
+    cursor = connection.cursor()
+
+    # Fetch the content from the database
+    cursor.execute(
+        f"""
+SELECT example.rowid, example.content 
+FROM example 
+JOIN link ON example.rowid = link.example_id 
+JOIN dataset ON link.dataset_id = dataset.id 
+WHERE dataset.name='{dataset}'
+AND example.rowid NOT IN (
+    SELECT example.rowid 
+    FROM example 
+    JOIN link ON example.rowid = link.example_id 
+    JOIN dataset ON link.dataset_id = dataset.id 
+    WHERE dataset.name='{str(dataset)+'_review'}'
+)
+    """
+    )
+    rows = cursor.fetchall()
+
+    # Loop through each row
+    for row in rows:
+        # Load the JSON
+        json_content = json.loads(row[1])
+        audio_id = json_content["id"]
+        audio_url = json_content["audio"]
+        text = json_content["transcript"]
+        yield {"id": audio_id, "audio": audio_url, "transcript": text}
