19d20b9811f3a37900fd361179a17c2fd5773dc2
kunsang <tenkus47@gmail.com>
2023-06-13 11:14:48 +0530

    add js file

--- recipes/stt_ab.py
+++ recipes/stt_ab.py
@@ -1,37 +1,47 @@
 import logging
 import prodigy
 import jsonlines
-
 # log config 
-logging.basicConfig(
-    filename="/usr/local/prodigy/logs/stt_ab.log",
-    format="%(levelname)s: %(message)s",
-    level=logging.INFO,
-    )
+# logging.basicConfig(
+#     filename="/usr/local/prodigy/logs/stt_ab.log",
+#     format="%(levelname)s: %(message)s",
+#     level=logging.INFO,
+#     )
 
 # Prodigy has a logger named "prodigy" according to 
 # https://support.prodi.gy/t/how-to-write-log-data-to-file/1427/10
-prodigy_logger = logging.getLogger('prodigy')
-prodigy_logger.setLevel(logging.INFO)
+# prodigy_logger = logging.getLogger('prodigy')
+# prodigy_logger.setLevel(logging.INFO)
+
+with open('recipes/js/auto.js') as txt:
+    script_text = txt.read()
 
 @prodigy.recipe("stt-ab-recipe")
 def stt_ab_recipe(dataset, jsonl_file):
-    logging.info(f"dataset:{dataset}, jsonl_file_path:{jsonl_file}")
+    # logging.info(f"dataset:{dataset}, jsonl_file_path:{jsonl_file}")
     blocks = [
         {"view_id": "audio"},
-        {"view_id": "text_input"},
+        {"view_id": "text_input",
+         "field_id": "user_input",
+         "field_placeholder": "Enter transcript here",
+         },
         {
             "view_id": "html",
             "html_template" : "<button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.5)'>0.5x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(0.7)'>0.7x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1)'>1x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.3)'>1.3x speed</button><button style='margin: 5px;' onclick='window.wavesurfer.setPlaybackRate(1.5)'>1.5x speed</button>"
         }
     ]
+    
+  
+    stream=stream_from_jsonl(jsonl_file)
     return {
         "dataset": dataset,
-        "stream": stream_from_jsonl(jsonl_file),
+        "stream": stream,
         "view_id": "blocks",
         "config": {
+            "title":"Pecha Tools",
             "blocks": blocks,
-            "editable": True
+            "editable": True,
+            "javascript":script_text,
         }
     }
 
