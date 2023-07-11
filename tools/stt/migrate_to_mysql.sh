a87fc96ce7348fb139698d5920ae3343aec8a225
Sonam Phuntsog <spsither@gmail.com>
2023-07-11 12:11:40 +0530

    migrate stt_tt_ge

--- tools/stt/migrate_to_mysql.sh
+++ tools/stt/migrate_to_mysql.sh
@@ -31,6 +31,13 @@ sudo -u prodigy PRODIGY_CONFIG="/usr/local/prodigy/prodigy-tools/configuration/s
 sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_gd ./stt_tt_gd.jsonl
 sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_gd_review ./stt_tt_gd_review.jsonl
 
+# stt_tt_ge
+sudo -u prodigy PRODIGY_CONFIG="/usr/local/prodigy/prodigy-tools/configuration/stt_tt_ge.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ge > ./stt_tt_ge.jsonl
+sudo -u prodigy PRODIGY_CONFIG="/usr/local/prodigy/prodigy-tools/configuration/stt_tt_ge_review.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ge_review > ./stt_tt_ge_review.jsonl
+
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_ge ./stt_tt_ge.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_ge_review ./stt_tt_ge_review.jsonl
+
 # stt_ns_ga
 sudo -u prodigy PRODIGY_CONFIG="./stt_ns_ga.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga > ./stt_ns_ga.jsonl
 sudo -u prodigy PRODIGY_CONFIG="./stt_ns_ga.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga_review > ./stt_ns_ga_review.jsonl
