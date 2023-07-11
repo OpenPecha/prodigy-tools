06f1837798de410069f6205faa0920c17135bc86
Sonam Phuntsog <spsither@gmail.com>
2023-07-11 11:40:24 +0530

    migrated stt_ns_ga

--- tools/stt/migrate_to_mysql.sh
+++ tools/stt/migrate_to_mysql.sh
@@ -23,3 +23,10 @@ sudo -u prodigy PRODIGY_CONFIG="/usr/local/prodigy/prodigy-tools/configuration/s
 
 sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_gc ./stt_tt_gc.jsonl
 sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_tt_gc_review ./stt_tt_gc_review.jsonl
+
+# stt_ns_ga
+sudo -u prodigy PRODIGY_CONFIG="./stt_ns_ga.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga > ./stt_ns_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./stt_ns_ga.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga_review > ./stt_ns_ga_review.jsonl
+
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_ns_ga ./stt_ns_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-in stt_ns_ga_review ./stt_ns_ga_review.jsonl
