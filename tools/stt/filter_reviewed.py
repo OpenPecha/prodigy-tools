45c2b1718262915ee4df01b8dd7abab0d827b708
Sonam Phuntsog <spsither@gmail.com>
2023-07-13 11:36:39 +0530

    check if new review works

--- tools/stt/filter_reviewed.py
+++ tools/stt/filter_reviewed.py
@@ -24,13 +24,13 @@ def filter_and_save_jsonl_ab(review_jsonl, transcript_jsonl, filtered_jsonl):
     transcript_df = transcript_pd.drop_duplicates(subset=['id'])
     
     transcript_df = transcript_df.dropna()
-    
+
     transcript_df = transcript_df[~transcript_df['id'].isin(review_df['id'])]
     
     with open(filtered_jsonl, 'w+', encoding='utf-8') as the_file:
         for i in range(len(transcript_df)):
             transcript = transcript_df.iloc[i]['transcript']
-            the_file.write(f"""{{"audio":"{transcript_df.iloc[i]['audio']}","url":"{transcript_df.iloc[i]['audio']}","text":"{transcript_df.iloc[i]['id']}","path":"{transcript_df.iloc[i]['audio']}","_input_hash":{transcript_df.iloc[i]['_input_hash']},"_task_hash":{transcript_df.iloc[i]['_task_hash']},"_is_binary":{transcript_df.iloc[i]['_is_binary']},"_view_id":"{transcript_df.iloc[i]['_view_id']}","transcript":"{transcript.encode('unicode-escape').decode('ascii')}","answer":"{transcript_df.iloc[i]['answer']}","_timestamp":{transcript_df.iloc[i]['_timestamp']},"_annotator_id":"{transcript_df.iloc[i]['_annotator_id']}","_session_id":"{transcript_df.iloc[i]['_session_id']}",}}\n""")
+            the_file.write(f"""{{"audio":"{transcript_df.iloc[i]['audio']}","url":"{transcript_df.iloc[i]['audio']}","text":"{transcript_df.iloc[i]['id']}","path":"{transcript_df.iloc[i]['audio']}","_input_hash":{transcript_df.iloc[i]['_input_hash']},"_task_hash":{transcript_df.iloc[i]['_task_hash']},"_view_id":"{transcript_df.iloc[i]['_view_id']}","transcript":"{transcript.encode('unicode-escape').decode('ascii')}","answer":"{transcript_df.iloc[i]['answer']}","_timestamp":{transcript_df.iloc[i]['_timestamp']},"_annotator_id":"{transcript_df.iloc[i]['_annotator_id']}","_session_id":"{transcript_df.iloc[i]['_session_id']}",}}\n""")
 
 
 filter_and_save_jsonl("/home/spsither/test/stt_tt_ga_review.jsonl", "/home/spsither/test/stt_tt_ga.jsonl", "/home/spsither/test/stt_tt_ga_filtered.jsonl")
@@ -45,7 +45,7 @@ filter_and_save_jsonl("/home/spsither/test/stt_tt_gh_review.jsonl", "/home/spsit
 filter_and_save_jsonl("/home/spsither/test/stt_ns_ga_review.jsonl", "/home/spsither/test/stt_ns_ga.jsonl", "/home/spsither/test/stt_ns_ga_filtered.jsonl")
 filter_and_save_jsonl("/home/spsither/test/stt_ns_gb_review.jsonl", "/home/spsither/test/stt_ns_gb.jsonl", "/home/spsither/test/stt_ns_gb_filtered.jsonl")
 filter_and_save_jsonl("/home/spsither/test/stt_ns_gc_review.jsonl", "/home/spsither/test/stt_ns_gc.jsonl", "/home/spsither/test/stt_ns_gc_filtered.jsonl")
-filter_and_save_jsonl("/home/spsither/test/stt_ns_gd_review.jsonl", "/home/spsither/test/stt_ns_gd.jsonl", "/home/spsither/test/stt_ns_gd_filtered.jsonl")
+#filter_and_save_jsonl("/home/spsither/test/stt_ns_gd_review.jsonl", "/home/spsither/test/stt_ns_gd.jsonl", "/home/spsither/test/stt_ns_gd_filtered.jsonl")
 
 filter_and_save_jsonl("/home/spsither/test/stt_cs_ga_review.jsonl", "/home/spsither/test/stt_cs_ga.jsonl", "/home/spsither/test/stt_cs_ga_filtered.jsonl")
 filter_and_save_jsonl("/home/spsither/test/stt_cs_gb_review.jsonl", "/home/spsither/test/stt_cs_gb.jsonl", "/home/spsither/test/stt_cs_gb_filtered.jsonl")
