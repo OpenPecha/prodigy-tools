2c888bccfa9ba058ab0101a51793ba2d6bf65404
Sonam Phuntsog <spsither@gmail.com>
2023-07-13 11:21:18 +0530

    export all dataset to jsonl filter all non reviewed ones

---
+++ tools/stt/export_all_jsonl.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# stt_tt_ga
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ga > ./stt_tt_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ga_review > ./stt_tt_ga_review.jsonl
+
+# stt_tt_gb
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gb > ./stt_tt_gb.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gb_review > ./stt_tt_gb_review.jsonl
+
+# stt_tt_gc
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gc > ./stt_tt_gc.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gc_review > ./stt_tt_gc_review.jsonl
+
+# stt_tt_gd
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gd > ./stt_tt_gd.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gd_review > ./stt_tt_gd_review.jsonl
+
+# stt_tt_ge
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ge > ./stt_tt_ge.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_ge_review > ./stt_tt_ge_review.jsonl
+
+# stt_tt_gf
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gf > ./stt_tt_gf.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gf_review > ./stt_tt_gf_review.jsonl
+
+# stt_tt_gg
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gg > ./stt_tt_gg.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gg_review > ./stt_tt_gg_review.jsonl
+
+# stt_tt_gh
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gh > ./stt_tt_gh.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_tt_gh_review > ./stt_tt_gh_review.jsonl
+
+# stt_ns_ga
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga > ./stt_ns_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_ga_review > ./stt_ns_ga_review.jsonl
+
+# stt_ns_gb
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gb > ./stt_ns_gb.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gb_review > ./stt_ns_gb_review.jsonl
+
+# stt_ns_gc
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gc > ./stt_ns_gc.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gc_review > ./stt_ns_gc_review.jsonl
+
+# stt_ns_gd
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gd > ./stt_ns_gd.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ns_gd_review > ./stt_ns_gd_review.jsonl
+
+# stt_cs_ga
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs > ./stt_cs_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_review > ./stt_cs_ga_review.jsonl
+
+# stt_cs_gb
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gb > ./stt_cs_gb.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gb_review > ./stt_cs_gb_review.jsonl
+
+# stt_cs_gc
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gc > ./stt_cs_gc.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gc_review > ./stt_cs_gc_review.jsonl
+
+# stt_cs_gd
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gd > ./stt_cs_gd.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gd_review > ./stt_cs_gd_review.jsonl
+
+# stt_cs_ge
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_ge > ./stt_cs_ge.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_ge_review > ./stt_cs_ge_review.jsonl
+
+# stt_cs_gf
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gf > ./stt_cs_gf.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_cs_gf_review > ./stt_cs_gf_review.jsonl
+
+# stt_ab_ga
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ab_ga > ./stt_ab_ga.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ab_ga_review > ./stt_ab_ga_review.jsonl
+
+# stt_ab_gb
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ab_gb > ./stt_ab_gb.jsonl
+sudo -u prodigy PRODIGY_CONFIG="./config_mysql.json" /usr/bin/python3.9 -m prodigy db-out stt_ab_gb_review > ./stt_ab_gb_review.jsonl
---
+++ tools/stt/filter_reviewed.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def filter_and_save_jsonl(review_jsonl, transcript_jsonl, filtered_jsonl):
+    review_pd = pd.read_json(path_or_buf=review_jsonl, lines=True)
+    transcript_pd = pd.read_json(path_or_buf=transcript_jsonl, lines=True)
+    
+    review_df = review_pd.drop_duplicates(subset=['text'])
+    transcript_df = transcript_pd.drop_duplicates(subset=['text'])
+    
+    transcript_df = transcript_df.dropna()
+
+    transcript_df = transcript_df[~transcript_df['text'].isin(review_df['text'])]
+    
+    with open(filtered_jsonl, 'w+', encoding='utf-8') as the_file:
+        for i in range(len(transcript_df)):
+            transcript = transcript_df.iloc[i]['transcript']
+            the_file.write(f"""{{"audio":"{transcript_df.iloc[i]['audio']}","text":"{transcript_df.iloc[i]['text']}","meta":{transcript_df.iloc[i]['meta']},"path":"{transcript_df.iloc[i]['audio']}","_input_hash":{transcript_df.iloc[i]['_input_hash']},"_task_hash":{transcript_df.iloc[i]['_task_hash']},"_is_binary":{transcript_df.iloc[i]['_is_binary']},"_view_id":"{transcript_df.iloc[i]['_view_id']}","transcript":"{transcript.encode('unicode-escape').decode('ascii')}","answer":"{transcript_df.iloc[i]['answer']}","_timestamp":{transcript_df.iloc[i]['_timestamp']},"_annotator_id":"{transcript_df.iloc[i]['_annotator_id']}","_session_id":"{transcript_df.iloc[i]['_session_id']}",}}\n""")
+
+def filter_and_save_jsonl_ab(review_jsonl, transcript_jsonl, filtered_jsonl):
+    review_pd = pd.read_json(path_or_buf=review_jsonl, lines=True)
+    transcript_pd = pd.read_json(path_or_buf=transcript_jsonl, lines=True)
+    
+    review_df = review_pd.drop_duplicates(subset=['id'])
+    transcript_df = transcript_pd.drop_duplicates(subset=['id'])
+    
+    transcript_df = transcript_df.dropna()
+    
+    transcript_df = transcript_df[~transcript_df['id'].isin(review_df['id'])]
+    
+    with open(filtered_jsonl, 'w+', encoding='utf-8') as the_file:
+        for i in range(len(transcript_df)):
+            transcript = transcript_df.iloc[i]['transcript']
+            the_file.write(f"""{{"audio":"{transcript_df.iloc[i]['audio']}","url":"{transcript_df.iloc[i]['audio']}","text":"{transcript_df.iloc[i]['id']}","path":"{transcript_df.iloc[i]['audio']}","_input_hash":{transcript_df.iloc[i]['_input_hash']},"_task_hash":{transcript_df.iloc[i]['_task_hash']},"_is_binary":{transcript_df.iloc[i]['_is_binary']},"_view_id":"{transcript_df.iloc[i]['_view_id']}","transcript":"{transcript.encode('unicode-escape').decode('ascii')}","answer":"{transcript_df.iloc[i]['answer']}","_timestamp":{transcript_df.iloc[i]['_timestamp']},"_annotator_id":"{transcript_df.iloc[i]['_annotator_id']}","_session_id":"{transcript_df.iloc[i]['_session_id']}",}}\n""")
+
+
+filter_and_save_jsonl("/home/spsither/test/stt_tt_ga_review.jsonl", "/home/spsither/test/stt_tt_ga.jsonl", "/home/spsither/test/stt_tt_ga_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gb_review.jsonl", "/home/spsither/test/stt_tt_gb.jsonl", "/home/spsither/test/stt_tt_gb_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gc_review.jsonl", "/home/spsither/test/stt_tt_gc.jsonl", "/home/spsither/test/stt_tt_gc_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gd_review.jsonl", "/home/spsither/test/stt_tt_gd.jsonl", "/home/spsither/test/stt_tt_gd_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_ge_review.jsonl", "/home/spsither/test/stt_tt_ge.jsonl", "/home/spsither/test/stt_tt_ge_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gf_review.jsonl", "/home/spsither/test/stt_tt_gf.jsonl", "/home/spsither/test/stt_tt_gf_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gg_review.jsonl", "/home/spsither/test/stt_tt_gg.jsonl", "/home/spsither/test/stt_tt_gg_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_tt_gh_review.jsonl", "/home/spsither/test/stt_tt_gh.jsonl", "/home/spsither/test/stt_tt_gh_filtered.jsonl")
+
+filter_and_save_jsonl("/home/spsither/test/stt_ns_ga_review.jsonl", "/home/spsither/test/stt_ns_ga.jsonl", "/home/spsither/test/stt_ns_ga_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_ns_gb_review.jsonl", "/home/spsither/test/stt_ns_gb.jsonl", "/home/spsither/test/stt_ns_gb_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_ns_gc_review.jsonl", "/home/spsither/test/stt_ns_gc.jsonl", "/home/spsither/test/stt_ns_gc_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_ns_gd_review.jsonl", "/home/spsither/test/stt_ns_gd.jsonl", "/home/spsither/test/stt_ns_gd_filtered.jsonl")
+
+filter_and_save_jsonl("/home/spsither/test/stt_cs_ga_review.jsonl", "/home/spsither/test/stt_cs_ga.jsonl", "/home/spsither/test/stt_cs_ga_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_cs_gb_review.jsonl", "/home/spsither/test/stt_cs_gb.jsonl", "/home/spsither/test/stt_cs_gb_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_cs_gc_review.jsonl", "/home/spsither/test/stt_cs_gc.jsonl", "/home/spsither/test/stt_cs_gc_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_cs_gd_review.jsonl", "/home/spsither/test/stt_cs_gd.jsonl", "/home/spsither/test/stt_cs_gd_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_cs_ge_review.jsonl", "/home/spsither/test/stt_cs_ge.jsonl", "/home/spsither/test/stt_cs_ge_filtered.jsonl")
+filter_and_save_jsonl("/home/spsither/test/stt_cs_gf_review.jsonl", "/home/spsither/test/stt_cs_gf.jsonl", "/home/spsither/test/stt_cs_gf_filtered.jsonl")
+
+filter_and_save_jsonl_ab("/home/spsither/test/stt_ab_ga_review.jsonl", "/home/spsither/test/stt_ab_ga.jsonl", "/home/spsither/test/stt_ab_ga_filtered.jsonl")
+filter_and_save_jsonl_ab("/home/spsither/test/stt_ab_gb_review.jsonl", "/home/spsither/test/stt_ab_gb.jsonl", "/home/spsither/test/stt_ab_gb_filtered.jsonl")
